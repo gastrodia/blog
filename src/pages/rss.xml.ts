ce7bb25b515e172da4d8aad0133d241a834ac3b1
gastrodia <3025822868@qq.com>
2025-02-20 03:42:11 +0000

    feat: new post

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,7 +4,7 @@ import getSortedPosts from "@utils/getSortedPosts";
 import slugify from "@utils/slugify";
 import { SITE } from "@config";
 
-export async function get() {
+export async function GET(context) {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
   return rss({
@@ -12,7 +12,7 @@ export async function get() {
     description: SITE.desc,
     site: SITE.website,
     items: sortedPosts.map(({ data }) => ({
-      link: `posts/${slugify(data)}`,
+      link: `/posts/${slugify(data)}`,
       title: data.title,
       description: data.description,
       pubDate: new Date(data.pubDatetime),
