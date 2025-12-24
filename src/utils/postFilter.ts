c68767b37f0ad94a8770c52f90d16ed0216305cf
gastrodia <3025822868@qq.com>
2025-12-24 06:58:03 +0000

    fix: timezone

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -1,7 +1,7 @@
 import type { CollectionEntry } from "astro:content";
 
 const postFilter = ({ data }: CollectionEntry<"blog">) => {
-  return !data.draft && import.meta.env.DEV;
+  return !data.draft || import.meta.env.DEV;
 };
 
 export default postFilter;
